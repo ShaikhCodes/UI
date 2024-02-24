194add575b5075c63a05c63acba11c0a13fc49b5
ShaikhCodes <91846097+ShaikhCodes@users.noreply.github.com>
2024-02-24 23:57:28 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
   const fetchTasks = async () => {
     try {
-      const response = await axios.get('http://65.1.168.37:5000/taskss');
+      const response = await axios.get('http://65.1.168.37:5000/tasks');
       setTasks(response.data);
     } catch (error) {
       console.error('Error fetching tasks:', error);
