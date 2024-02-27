90ea0e74817afeac56d7c35a4f94332804c95f48
ShaikhCodes <91846097+ShaikhCodes@users.noreply.github.com>
2024-02-27 15:21:03 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
   const fetchTasks = async () => {
     try {
-      const response = await axios.get(`${process.env.backend_ip}/tasks`);
+      const response = await axios.get(`${process.env.bip}/tasks`);
       setTasks(response.data);
     } catch (error) {
       console.error('Error fetching tasks:', error);
@@ -21,7 +21,7 @@ export default function Home() {
 
   const addTask = async () => {
     try {
-      await axios.post(process.env.backend_ip/tasks, { task: newTask });
+      await axios.post(process.env.bip/tasks, { task: newTask });
       setNewTask('');
       fetchTasks();
     } catch (error) {
@@ -31,7 +31,7 @@ export default function Home() {
 
   const deleteTask = async (taskId) => {
     try {
-      await axios.delete(`${process.env.backend_ip}/tasks/${taskId}`);
+      await axios.delete(`${process.env.bip}/tasks/${taskId}`);
       fetchTasks();
     } catch (error) {
       console.error('Error deleting task:', error);
